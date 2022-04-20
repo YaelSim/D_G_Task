18c522c18dec60c80e257e327a90e2bf004d37a0
YaelSim <58948260+YaelSim@users.noreply.github.com>
2022-04-20 21:54:26 +0300

    Updates

---
+++ precache-manifest.ec38efb7d075b91a137e9ddae8461b05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f988746a41c6aa25c6eb",
+    "url": "/D_G_Task/static/css/main.91fd5551.chunk.css"
+  },
+  {
+    "revision": "f988746a41c6aa25c6eb",
+    "url": "/D_G_Task/static/js/main.f988746a.chunk.js"
+  },
+  {
+    "revision": "c9a6fbe47b7867e1be38",
+    "url": "/D_G_Task/static/js/1.c9a6fbe4.chunk.js"
+  },
+  {
+    "revision": "7adf6af61722c0b5044f",
+    "url": "/D_G_Task/static/js/runtime~main.7adf6af6.js"
+  },
+  {
+    "revision": "06bde6a8f4cd64f5ceefaf4def26dc88",
+    "url": "/D_G_Task/index.html"
+  }
+];
